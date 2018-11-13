cd26f087def52f6f8ca700024c14387eeaa55d67
longvity <1563086186@qq.com>
2018-11-13 17:06:16 +0800

    项目初始化 集成脚本管理器 实现状态保持 session 信息存储

--- manage.py
+++ manage.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask,session
+
+from flask_script import  Manager
+
+from flask_session import  Session
+
+
+from redis import  StrictRedis
+
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY']='9CDgVJOpsP6pPdDfh+IAVxryAIj7ofuGCogzGCUS'
+app.config['SESSION_TYPE']='redis'
+app.config['SESSION_REDIS']=StrictRedis(host='127.0.0.1',port=6379)
+app.config['SEEION_USE_SIGNER']=True
+app.config['PERMANENT_SESSION_LIFETIME']=86400
+
+manage=Manager(app)
+Session()
 
 @app.route('/')
 
@@ -12,4 +28,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    manage.run()
