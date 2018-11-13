d610677afaf5ea49b427a43305a1d51d37c5cb96
longvity <1563086186@qq.com>
2018-11-13 17:28:30 +0800

    mysql 数据库的配置 迁移扩展包

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 from flask import Flask,session
 
 from flask_script import  Manager
+from flask_sqlalchemy import  SQLAlchemy
+from flask_migrate import  Migrate,MigrateCommand
+
 
 from flask_session import  Session
 
@@ -11,12 +14,16 @@ from redis import  StrictRedis
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='9CDgVJOpsP6pPdDfh+IAVxryAIj7ofuGCogzGCUS'
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:mysql/localhost/info18'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SESSION_TYPE']='redis'
 app.config['SESSION_REDIS']=StrictRedis(host='127.0.0.1',port=6379)
 app.config['SEEION_USE_SIGNER']=True
 app.config['PERMANENT_SESSION_LIFETIME']=86400
-
+db =SQLAlchemy(app)
+Migrate = Migrate(app)
 manage=Manager(app)
+manage.add_command('db',MigrateCommand)
 Session()
 
 @app.route('/')
